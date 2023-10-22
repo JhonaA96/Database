49a13c399ca41963ddafac293e99fd48aa8affad
Jhonatan Acuña <jhonatanacuna@cbit-online.com>
2023-10-22 15:42:06 -0500

    First Commit

--- BI/Script-10.sql
+++ BI/Script-10.sql
@@ -14,17 +14,42 @@ GROUP BY YEAR(sod.ModifiedDate), p.Name, pc.Name
 ORDER BY pc.Name ASC, p.Name ASC, YEAR (sod.ModifiedDate) ASC;
 
 --2. Un query que permita calcular el margen de beneficio de cada producto en el último año:
+
+Quantity_Selled 
+Total_Cost (SUM(sod.OrderQty) * p.StandardCost)
+unit_price 
+total_selled SUM(sod.LineTotal) 
+
+(SUM(sod.LineTotal) - (SUM(sod.OrderQty) * p.StandardCost)) / SUM(sod.LineTotal)
+
 SELECT 
 	p.Name AS Product,
-	SUM(sod.LineTotal/sod.OrderQty) AS total_selled,
-	p.StandardCost AS Cost,
-	SUM((sod.LineTotal/sod.OrderQty) - p.StandardCost)
+	--p.StandardCost as Cost,
+	--SUM(sod.OrderQty) AS Quantity_Selled,
+	--FORMAT(SUM(sod.OrderQty) * p.StandardCost, 'C', 'En-us') AS Total_Cost,
+	--FORMAT(SUM(sod.LineTotal) / SUM(sod.OrderQty), 'C', 'En-us') AS unit_price,
+	--FORMAT(SUM(sod.LineTotal), 'C', 'En-us') as total_selled,
+	(SUM(sod.LineTotal) - (SUM(sod.OrderQty) * p.StandardCost)) / SUM(sod.LineTotal) as Profit_Margin
 FROM Sales.SalesOrderDetail sod
-INNER JOIN Production.Product p ON sod.ProductID = p.ProductID
-GROUP BY p.Name, p.StandardCost;
+INNER JOIN Sales.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID
+INNER JOIN Production.Product p ON sod.ProductID  = p.ProductID
+WHERE YEAR(soh.OrderDate) = 2014
+group by p.Name, p.StandardCost;
 
-SELECT * FROM Production.Product p
 --3. Un query que permita calcular el tiempo promedio de envío por categoría de productos en el último trimestre:
+
+SELECT 
+	pc.Name as Category_Name,
+	AVG(DATEDIFF(day, soh.OrderDate, soh.ShipDate)) AS Averager_Days
+FROM Sales.SalesOrderDetail sod
+INNER JOIN Sales.SalesOrderHeader soh ON sod.SalesOrderID = soh.SalesOrderID
+INNER JOIN Production.Product p ON sod.ProductID = p.ProductID
+INNER JOIN Production.ProductSubcategory ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+INNER JOIN Production.ProductCategory pc ON ps.ProductCategoryID = pc.ProductCategoryID
+WHERE YEAR(soh.OrderDate) = 2014
+AND DATEPART(qq, soh.OrderDate) = 2
+GROUP BY pc.Name, soh.OrderDate;
+
 --4. Un query que permita calcular el tiempo promedio de envío por categoría de productos en el último trimestre, incluyendo información sobre clientes, empleados y proveedores.
 --5. Un query que permita analizar el desempeño de los productos en función de las revisiones de los clientes, mostrando información sobre empleados y clientes asociados a las revisiones
 --6. Un query que permita identificar los productos más devueltos en el último mes, incluyendo información sobre los clientes y empleados asociados a las devoluciones
